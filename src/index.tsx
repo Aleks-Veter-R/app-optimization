3640ffdf93a372dbc1a67e9f5b93b0bcdf31fcd3
Aleksander <leks.creative.mail@gmail.com>
2025-02-14 13:52:48 +0300

    AO-001 Подключение локального сервера msw (MockServerWorker)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,23 @@
 import { createRoot } from 'react-dom/client';
+import App from './App';
+
+async function enableMocking() {
+    if (process.env.NODE_ENV === 'development') {
+        const { server } = await import('../mocks/node');
+
+        return server.start({ quiet: true });
+    }
+
+    return Promise.resolve();
+}
 
 const rootElement = document.getElementById('root');
 const rootContainer = createRoot(rootElement);
 
-rootContainer.render(
-    <div>
-        React project!
-    </div>,
-);
+enableMocking().then(() => {
+    rootContainer.render(
+        <div>
+            <App />
+        </div>,
+    ); 
+});
