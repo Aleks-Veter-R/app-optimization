226baaaedfae7df78cf83825964cf8ed137f236f
Aleksander <leks.creative.mail@gmail.com>
2025-03-13 12:52:27 +0300

    AO-007 React Lazy оптимизация

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 import Root from './components/Root';
 import { Main } from './components/Main';
@@ -10,9 +11,16 @@ const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Root />}>
+                <Route path='/' element={<Root />}>
                     <Route index element={<Main />} />
-                    <Route path="second-page" element={<Second />} />
+                    <Route
+                        path='second-page'
+                        element={
+                            <Suspense fallback='Загрузка страницы...'>
+                                <Second />
+                            </Suspense>
+                        }
+                    />
                 </Route>
             </Routes>
         </BrowserRouter>
